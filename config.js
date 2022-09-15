f2fe706204500ed53f086332e8a013e963aab055
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-15 12:53:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['16199961931', 'Ziv San', true], 
-['6282179137771', 'HAORI IQ-MD', true] 
+['66615143033', 'Sad Human', true], 
+['6281335334848', 'HAORI IQ-MD', true] 
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
